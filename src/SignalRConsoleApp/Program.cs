b2519cccc1ee6fe9c8f054b1469b70d4ca71ac59
yeaiyu <yeaiyu@outlook.com>
2024-08-27 16:46:42 +0800

    signalr

---
+++ src/SignalRConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Run();
+    }
+
+    private static async void Run()
+    {
+        HubConnection con = new HubConnectionBuilder()
+            .WithUrl("https://localhost:62814/talks")
+            //.WithAutomaticReconnect()
+            .Build();
+
+        con.Closed += async error =>
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await con.StartAsync();
+        };
+
+        con.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            Console.WriteLine($"{user}->{message}");
+        });
+
+        try
+        {
+            await con.StartAsync();
+
+            await con.InvokeAsync("SendMessage", "管理员", "hello 管理员。");
+        }
+        catch (Exception ex)
+        {
+        }
+        finally
+        {
+
+        }
+
+        Console.ReadKey();
+    }
+}
